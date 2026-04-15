e55cbef96672c1abe8c9d1a294eefc1f46b9e12c
Caio Bertolato <bertolatopereiracaio@gmail.com>
2026-04-15 16:01:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// script.js
+let map, directionsService, directionsRenderer;
 
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { DirectionsService, DirectionsRenderer } = await google.maps.importLibrary("routes");
+  await google.maps.importLibrary("geometry");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -19.9, lng: -44.0 },
+    zoom: 8
+  });
+
+  directionsService = new DirectionsService();
+  directionsRenderer = new DirectionsRenderer({ map });
+}
+
+initMap();
