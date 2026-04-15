af825b48798068ad80a4df9791eaf58846d889aa
Caio Bertolato <bertolatopereiracaio@gmail.com>
2026-04-15 16:01:38 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,5 @@
-
+// data.js
+const fornecedores = [
+  { nome: "TREVISO BETIM", lat: -19.961916, lng: -44.164528, cidade: "BETIM" },
+  { nome: "STG", lat: -19.960611, lng: -44.075444, cidade: "BETIM" }
+];
